1cee02ab4c8d776c72a3c31825e718b72383c439
Gregorio <grex@gsyc.urjc.es>
2021-03-05 15:39:48 +0100

    A little bit of projedct cleaning, including PEP8 and other warnings.

--- duplicateScripts.py
+++ duplicateScripts.py
@@ -2,178 +2,163 @@
 import json
 import zipfile
 
-N_BLOCKS = 3
+N_BLOCKS = 4
 
-class DuplicateScripts():
-
-   """Analyzer of duplicate scripts in projects sb3, the new version Scratch 3.0"""
-
-   def __init__(self):
-
-     self.total_duplicate = 0
-     self.blocks_dicc = {}
-     self.total_blocks = []
-     self.list_duplicate = []
-     self.blocks_dup = {}
-     self.loop_blocks = ["control_repeat", "control_forever", "control_if", "control_if_else", "control_repeat_until"]
-     #self.list_duplicate_string = []
+LOOP_BLOCKS = ["control_repeat", "control_forever", "control_if", "control_if_else", "control_repeat_until"]
 
 
-   """Only takes into account scripts with more than 5 blocks"""
-   def analyze(self, filename):
+class DuplicateScripts:
+    """
+    Analyzer of duplicate scripts in sb3 projects sb3
+    New version for Scratch 3.0
+    """
 
-     zip_file = zipfile.ZipFile(filename, "r")
-     json_project = json.loads(zip_file.open("project.json").read())
+    def __init__(self):
+        self.blocks_dict = {}
+        self.total_blocks = []
+        self.list_duplicate = []
+        self.blocks_dup = {}
+        #  self.list_duplicate_string = []
+
+    def analyze(self, filename):
+        """Only takes into account scripts with more than 5 blocks"""
+        zip_file = zipfile.ZipFile(filename, "r")
+        json_project = json.loads(zip_file.open("project.json").read())
   
-     scripts_set = {}
+        scripts_set = {}
 
-     for key, value in json_project.iteritems():
-       print(key)
-       if key == "targets":
-         for dicc in value:
-           sprite = dicc["name"]
-           self.blocks_dicc = {}
-           scripts_set[sprite] = []
+        for key, value in json_project.items():
+            print(key)
+            if key == "targets":
+                for dictionary in value:
+                    sprite = dictionary["name"]
+                    self.blocks_dict = {}
+                    scripts_set[sprite] = []
 
-           for blocks, blocks_value in dicc["blocks"].iteritems():
-               # for blocks, blocks_value in dicc_value.iteritems():
-                 if type(blocks_value) is dict:
-                   self.blocks_dicc[blocks] = blocks_value
-                   self.total_blocks.append(blocks_value)
+                    for blocks, blocks_value in dictionary["blocks"].items():
+                        # for blocks, blocks_value in dict_value.iteritems():
+                        if isinstance(blocks_value, dict):
+                            self.blocks_dict[blocks] = blocks_value
+                            self.total_blocks.append(blocks_value)
 
-           for key_block in self.blocks_dicc:
-               block = self.blocks_dicc[key_block]
+                    for key_block in self.blocks_dict:
+                        block = self.blocks_dict[key_block]
 
-               if block["topLevel"] == True:
-                  block_list = []
-                  next = block["next"]
-                  aux_next = []
-                  else_block = None
-                  self.search_next(next, block_list, key_block, aux_next, else_block)
+                        if block["topLevel"]:
+                            block_list = []
+                            next_block = block["next"]
+                            aux_next = []
+                            else_block = None
+                            self.search_next(next_block, block_list, key_block, aux_next, else_block)
 
-                  blocks_tuple = tuple(block_list)
+                            blocks_tuple = tuple(block_list)
 
-                  for sprite_key, sprite_value in scripts_set.iteritems():
-                    if blocks_tuple in sprite_value:
-                        self.list_duplicate.append(block_list)
-                        self.total_duplicate += 1
+                            for sprite_key, sprite_value in scripts_set.items():
+                                if blocks_tuple in sprite_value:
+                                    self.list_duplicate.append(block_list)
 
-                  # Only save the scripts with more than 5 blocks
-                  if len(block_list) >= N_BLOCKS:
-                    scripts_set[sprite].append(blocks_tuple)
-     print(scripts_set)
+                            # Only save the scripts with more than N_BLOCKS blocks
+                            if len(block_list) >= N_BLOCKS:
+                                scripts_set[sprite].append(blocks_tuple)
+        print(scripts_set)
 
+        #  Find the number of duplicates
+        for repeat_block in self.list_duplicate:
+            sprites_dup = []
 
-     #Find the number of duplicates
-     for repeat_block in self.list_duplicate:
-        sprites_dup = []
+            for key, value in scripts_set.items():
+                if tuple(repeat_block) in value:
+                    sprites_dup.append(str(key))
 
-        for key, value in scripts_set.iteritems():
-            if tuple(repeat_block) in value:
-               sprites_dup.append(str(key))
+            sprites_dup = ", ".join(sprites_dup)
+            if sprites_dup not in self.blocks_dup:
+                self.blocks_dup[sprites_dup] = []
 
-        sprites_dup = ", ".join(sprites_dup)
-        if sprites_dup not in self.blocks_dup:
-            self.blocks_dup[sprites_dup] = []
+            self.blocks_dup[sprites_dup].append(repeat_block)
 
-        self.blocks_dup[sprites_dup].append(repeat_block)
+    def search_next(self, next_block, block_list, key_block, aux_next, else_block):
 
+        block = self.blocks_dict[key_block]
+        block_list.append(block["opcode"])
 
+        # Check if it's if_else block
+        try:
+            is_else = block["inputs"]["SUBSTACK2"][1]
+        except KeyError:
+            is_else = None
 
-   def search_next(self, next, block_list, key_block, aux_next, else_block):
+        if not next_block:
+            # Maybe is a loop block
+            try:
+                next_block = self.blocks_dict[key_block]["inputs"]["SUBSTACK"][1]
+            except KeyError:
+                print("KeyError")
+            if not next_block:
+                block_list.append("finish_end")
+                return
 
-       block = self.blocks_dicc[key_block]
-       block_list.append(block["opcode"])
+            block = self.blocks_dict[next_block]
+            key_block = next_block
+            next_block = block["next"]
 
-       # Check if it's if_else block
-       try:
-           is_else = block["inputs"]["SUBSTACK2"][1]
-       except:
-           is_else = None
-           pass
+            self.search_next(next_block, block_list, key_block, aux_next, is_else)
+            block_list.append("finish_end")
 
-       if next == None:
-           try:
-              # Maybe is a loop block
-              next = self.blocks_dicc[key_block]["inputs"]["SUBSTACK"][1]
-              if next == None:
-                  block_list.append("finish_end")
-                  return
-
-              block = self.blocks_dicc[next]
-              key_block = next
-              next = block["next"]
-
-              self.search_next(next, block_list, key_block, aux_next, is_else)
-              block_list.append("finish_end")
-           except:
-              pass
-
-           if else_block:
-                next = else_block
+            if else_block:
+                next_block = else_block
                 else_block = None
                 block_list.append("control_else")
-           else:
+            else:
                 return
-       else:
-            # Maybe is a loop block
-            is_loop = any(self.blocks_dicc[key_block]["opcode"] == loop for loop in self.loop_blocks)
+        else:
+            # Maybe it is a loop block
+            is_loop = any(self.blocks_dict[key_block]["opcode"] == loop for loop in LOOP_BLOCKS)
             if is_loop:
-                try:
-                    loop_block = self.blocks_dicc[key_block]["inputs"]["SUBSTACK"][1]
-                    aux_next.append(next)          #Add the real next until the end of the loop
-                    next = loop_block
+                loop_block = self.blocks_dict[key_block]["inputs"]["SUBSTACK"][1]
+                aux_next.append(next_block)  # Add the real next until the end of the loop
+                next_block = loop_block
 
-                    block = self.blocks_dicc[next]
-                    key_block = next
-                    next = block["next"]
-                    self.search_next(next, block_list, key_block, aux_next, is_else)
+                if next_block:
+                    block = self.blocks_dict[next_block]
+                    key_block = next_block
+                    next_block = block["next"]
+                    self.search_next(next_block, block_list, key_block, aux_next, is_else)
 
                     block_list.append("finish_end")
-                    if aux_next:      #Check if there is an aux_next saved
-                          next = aux_next[-1]
-                          aux_next.pop(-1)
+                    if aux_next:  # Check if there is an aux_next saved
+                        next_block = aux_next[-1]
+                        aux_next.pop(-1)
                     else:
-                          return
-                except:
-                    pass
+                        return
 
+        if next_block:
+            block = self.blocks_dict[next_block]
+            key_block = next_block
+            next_block = block["next"]
 
-       if next:
-           block = self.blocks_dicc[next]
-           key_block = next
-           next = block["next"]
+            self.search_next(next_block, block_list, key_block, aux_next, else_block)
 
-           self.search_next(next, block_list, key_block, aux_next, else_block)
+    def finalize(self):
+        """Output the duplicate scripts detected."""
 
-       return
+        result = ("%d duplicate scripts found" % len(self.list_duplicate))
+        result += "\n"
+        result += str(self.blocks_dup)
 
-
-
-   """Output the duplicate scripts detected."""
-   def finalize(self):
-
-     result = ("%d duplicate scripts found" % self.total_duplicate)
-     result += "\n"
-     result += str(self.blocks_dup)
-
-     return result
-
+        return result
 
 
 def main(filename):
     """The entrypoint for the 'duplicateScripts' extension"""
-
- 
     duplicate = DuplicateScripts()
     duplicate.analyze(filename)
-    #print(duplicate.finalize())
+#    print(duplicate.finalize())
 
-from os import listdir
-from os.path import isfile, join
-mypath = "./projects/"
-new_path = "./sb3/"
-"""onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-for file in onlyfiles:
-    main("./projects/" + file)"""
-main("test.sb3")
+
+if __name__ == "__main__":
+    my_path = "./projects/"
+    new_path = "./sb3/"
+    """only_files = [f for f in listdir(my_path) if isfile(join(my_path, f))]
+    for file in only_files:
+        main("./projects/" + file)"""
+    main("test.sb3")
