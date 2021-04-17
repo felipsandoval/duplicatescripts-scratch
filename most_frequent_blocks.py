0f3929ac1225ee57b941b55cd9b749991385caa2
Felipe Sandoval Sibada <felipesandoval1111@gmail.com>
2021-04-17 21:25:12 +0200

    Now working

--- most_frequent_blocks.py
+++ most_frequent_blocks.py
@@ -10,33 +10,35 @@ import string
 
 def main (filename):
     d = defaultdict(int)
-    ordered = OrderedDict()
+    ordered_words = OrderedDict()
+    ordered_frequency = OrderedDict()
 
     characters = string.ascii_letters + string.punctuation + string.digits
     characters += "€£ñÑçÇáÁéÉíÍóÓúÚäÄëËïÏöÖüÜàÀèÈìÌòÒùÙâÂêÊîÎôÔûÛ¶§©ŠØ®ГДЕЖИЛśĥčýÿў±žПШΘЯбψξλσαβδ"
 
-    #mypath = sys.argv[1]
-    #_, _, filenames = next(walk(mypath))
-    #for filename in filenames:
-    #    json_project = json.loads(open(mypath + filename).read())
     json_project = json.loads(open(filename).read())
     # Loops through all sprites
     for sprites_dict in json_project["targets"]:
-        sprite = sprites_dict["name"]
-
         # Gets all blocks out of sprite
         for blocks, blocks_value in sprites_dict["blocks"].items():
             try:
                 d[blocks_value['opcode']] += 1
+                #Intentar ordenarlos no por número sino por colores y
+                #en hexadecimales pej
+                #print(d)
             except TypeError:
+                print("entre aqui")
                 pass
-
+    #print(d.items())
     most_frequent = sorted(d.items(), key=lambda kv: kv[1], reverse=True)
+    #print(most_frequent)
     for block, frequency in most_frequent:
-        ordered[block] = characters[len(ordered)]
+        ordered_words[block] = characters[len(ordered_words)]
+        #print(ordered_words)
 
     with open('blocks.json', 'w') as outfile:
-        json.dump(dict(ordered), outfile, indent=4)
+        json.dump(dict(ordered_words), outfile, indent=4)
 
 if __name__ == "__main__":
-    main(sys.argv[1])    
+    main(sys.argv[1])
+    
