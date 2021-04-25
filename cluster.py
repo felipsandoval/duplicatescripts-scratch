aa5da7b162cbba6750608e9b860c56e014058e9b
Felipe Sandoval Sibada <felipesandovalsibada@outlook.com>
2021-04-25 12:32:57 +0200

    Exceptions for importing module errors

--- cluster.py
+++ cluster.py
@@ -1,10 +1,32 @@
 #!/usr/bin/python3.8
-import numpy as np
-from sklearn.cluster import AffinityPropagation
-import distance
 from collections import Counter
 from statistics import json2dna
 import sys
+import subprocess
+import sys
+from pip._internal import main as pip
+import sklearn
+try:
+    import numpy as np
+except ImportError:
+    pip(['install', '--user', 'numpy'])
+    import numpy as np
+try:
+    import distance
+except ImportError:
+    pip(['install', '--user', 'distance'])
+    import distance
+#try:
+#    from sklearn.cluster import AffinityPropagation
+#except ImportError:
+#    pip(['install', '--user', 'scikit-learn'])
+#    pip(['install', '--user', 'sklearn.cluster'])
+#    pip(['install', '--user', 'spicy'])
+#    pip(['install', '--user', 'matplotlib'])
+#    from sklearn.cluster import AffinityPropagation
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 def main (filename):
     (scripts, blocks_dict) = json2dna(filename)
@@ -19,7 +41,7 @@ def main (filename):
     words = np.asarray(words)  # So that indexing with a list will work
     lev_similarity = -1 * np.array([[distance.levenshtein(w1, w2) for w1 in words] for w2 in words])
 
-    affprop = AffinityPropagation(affinity="precomputed", damping=0.75, random_state=None)
+    affprop = sklearn.cluster.AffinityPropagation(affinity="precomputed", damping=0.75, random_state=None)
     affprop.fit(lev_similarity)
     for cluster_id in np.unique(affprop.labels_):
         exemplar = words[affprop.cluster_centers_indices_[cluster_id]]
