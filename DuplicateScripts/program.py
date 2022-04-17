87c7418ee0f9c4ba2e8849b18bb6f3f11fb6fb2d
Felipe Sandoval Sibada <felipesandovalsibada@outlook.com>
2022-04-17 20:32:42 +0200

    Simplifying

--- DuplicateScripts/program.py
+++ DuplicateScripts/program.py
@@ -30,30 +30,6 @@ def sb3_json_extraction(fileIn):
     # os.remove(fileOut)
     return json_project
 
-def obtaining_json(filename):
-    """Obtains JSON file from different extentions"""
-    try:
-        json_files_list = []
-        if filename.endswith(".zip"):
-            # Creates a list with all JSON filenames
-            zip_file = zipfile.ZipFile(filename, "r")
-            listOfFileNames = zip_file.namelist()
-            for file in listOfFileNames:
-                if file.endswith('.json'):
-                   # zip_file.extract(file)
-                    json_files_list.append(file)
-            return json_files_list
-        elif filename.endswith(".json"):
-            json_file = json.loads(open(filename).read())
-        elif filename.endswith(".sb3"):
-            json_file = sb3_json_extraction(filename)
-    except FileNotFoundError:
-        sys.exit("\nPlease, use a file that exists in directory\n")
-    except:
-        sys.exit("\nPlease, use a valid extension file like .SB3," +
-        " JSON or .ZIP\n")
-    return json_file
-
 def define_duplicates(filename, json_file, ignoring):
     """
     Defines DuplicateScripts class and gives feedback
@@ -69,8 +45,6 @@ def define_duplicates(filename, json_file, ignoring):
 def main(filename, ignoring):
     """MAIN PROGRAM"""
     print("\n-- STARTING ANALYSIS --\n")
-    json_file = obtaining_json(filename)
-    print(json_file)
     duplicateScripts.main(filename, ignoring)
     most_frequent_blocks.main(filename)
     spritefile = filename.replace('.json', '') + '-sprite.json'
@@ -97,5 +71,5 @@ if __name__ == "__main__":
                  "\n-i (OPTIONAL): Ignore blocks from IgnoreBlocks.txt\n")
     except FileNotFoundError:
         sys.exit("\nPlease, use a file that exists in directory\n")
-    except:
-        sys.exit("\nSomething unexpected happened: ", sys.exc_info()[0])
+    #except:
+    #    sys.exit("\nSomething unexpected happened: ", sys.exc_info()[0])
