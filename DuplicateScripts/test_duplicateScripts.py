af0c222ff252d77a53568493735da2e93f5ccec7
Felipe Sandoval Sibada <felipesandoval1111@gmail.com>
2022-06-18 13:20:17 +0200

    Update test_duplicateScripts.py

--- DuplicateScripts/test_duplicateScripts.py
+++ DuplicateScripts/test_duplicateScripts.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # Github: @felipsandoval
-# para ejecutar: python3 -m unittest <nombre del fichero test_XXX.py>
+# To run with module: python3 -m unittest <test_XXX.py>
+# Also run with: python3 <test_XXX.py>
+# Run all test with: python -m unittest discover
 
 from duplicateScripts import *
 import unittest
 
+
 class TestCuboid(unittest.TestCase):
     def test_volume(self):
-        self.assertAlmostEqual(cuboid_volume(2),8)
-        self.assertAlmostEqual(cuboid_volume(1),1)
-        self.assertAlmostEqual(cuboid_volume(0),0)
-    
+        self.assertAlmostEqual(cuboid_volume(2), 8)
+        self.assertAlmostEqual(cuboid_volume(1), 1)
+        self.assertAlmostEqual(cuboid_volume(0), 0)
+
     def test_input_value(self):
         self.assertRaises(TypeError, cuboid_volume, True)
 
-#unittest.main(argv=[''],verbosity=2, exit=False)
+
+if __name__ == "__main__":
+    unittest.main()
